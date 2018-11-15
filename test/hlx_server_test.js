918214b0cdb0c8284e12716cc3df581deab4c5b8
Alexandre Capt <acapt@adobe.com>
2018-11-15 15:28:54 +0100

    Test that simulator can support other extension than html

--- test/hlx_server_test.js
+++ test/hlx_server_test.js
@@ -122,6 +122,21 @@ describe('Helix Server', () => {
     }
   });
 
+  it('deliver rendered json resource', async () => {
+    const cwd = path.join(SPEC_ROOT, 'local');
+    const project = new HelixProject()
+      .withCwd(cwd)
+      .withBuildDir('./build')
+      .withHttpPort(0);
+    await project.init();
+    try {
+      await project.start();
+      await assertHttp(`http://localhost:${project.server.port}/index.json`, 200, 'expected_index.json');
+    } finally {
+      await project.stop();
+    }
+  });
+
   it('deliver request headers', async () => {
     const cwd = path.join(SPEC_ROOT, 'local');
     const project = new HelixProject()
