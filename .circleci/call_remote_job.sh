a17105180cb4a55e6ccc19db71745ac08736d648
Alexandre Capt <acapt@adobe.com>
2018-11-21 14:43:09 +0100

    Trying to call full build

--- .circleci/call_remote_job.sh
+++ .circleci/call_remote_job.sh
@@ -7,10 +7,9 @@
 # $5 = REMOTE_BRANCH
 # $6 = smoke_job.json
 
-
 curl \
     --user $1: \
     --header "Content-Type: application/json" \
     --silent \
-    --data "{\"build_parameters\": {\"CIRCLE_JOB\": \"$2\", \"GDM_MODULE_BRANCHES\": { \"${CIRCLE_PROJECT_REPONAME}\": \"$3\"}}}" \
+    --data "{\"build_parameters\": {\"GDM_MODULE_BRANCHES\": { \"${CIRCLE_PROJECT_REPONAME}\": \"$3\"}}}" \
     --request POST "https://circleci.com/api/v1.1/project/github/$4/tree/$5" > $6
